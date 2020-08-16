8be94a67e886c07f7b00feab0264944fc4efbef0
Konstantin Oreshkin <oreshkin.konstantin@gmail.com>
2020-08-16 12:44:45 +0300

    init

---
+++ src/app/activity.ts
@@ -0,0 +1,37 @@
+export const activities = [
+  {
+    type: 'Work',
+    blocksCount: 60,
+    color: '#F28972'
+  },
+  {
+    type: 'Sleep',
+    blocksCount: 51,
+    color: '#F2B591'
+  },
+  {
+    type: 'Meditation',
+    blocksCount: 2,
+    color: '#F2D2B6',
+  },
+  {
+    type: 'Social',
+    blocksCount: 12,
+    color: '#194159',
+  },
+  {
+    type: 'Reading',
+    blocksCount: 2,
+    color: '#D4DBF5'
+  },
+  {
+    type: 'Eating',
+    blocksCount: 6,
+    color: '#8D07F6',
+  },
+  {
+    type: 'Sport',
+    blocksCount: 3,
+    color: '#0A1747',
+  },
+];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { activities } from './activity';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blocks';
+  activities = activities;
+  totalBlocks = 144;
+  blocks = [];
+  busyBlocks = 0;
+
+  constructor() {
+    this.blocks = Array(this.totalBlocks).fill(0).map((x,i)=>i);
+    this.busyBlocks = this.activities.reduce(
+      (accumulator, activity) => {
+        return accumulator + activity.blocksCount;
+      }, 0
+    )
+  }
 }
